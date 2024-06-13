9750869d5ef73d974aa5bf1d226289be06b9ae5a
cpt-source <checkpointthree.dev@gmail.com>
2024-06-12 19:19:19 -0700

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1952863.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 171826094, 167769504, 167769523, 167769525, 167769520, 167769503, 167769457, 167769540, 167769526, 167769517, 167769529, 167769532, 167769438, 167769516, 167769508, 167769511, 167769537, 167769543, 171825965 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/167769541/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/167769435/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/167769542/1/ammo.js', 'preload' : false},
+];
